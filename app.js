645ac56f49ed812e36064f6d646d628938b44105
Indio Ferrel <mferrel1911@gmail.com>
2022-05-06 11:25:24 -0700

    Implemented javascript and html elements for location and stats, updated font in home.css.

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 // import functions and grab DOM elements
+const locationSelector = document.getElementById('locationSelect');
+const locationDiv = document.getElementById('location');
+
+const reportEl = document.getElementById('report');
 
 // let state
+let locationCount = 0;
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+// event listeners
+locationSelector.addEventListener('change', () => {
+    locationCount++;
+    locationDiv.style.backgroundImage = `url('./assets/${locationSelector.value}farthing.jpg')`;
+    displayStats();
+});
+
+// called functions
+
+function displayStats() {
+    reportEl.textContent = `You have changed the location ${locationCount} times, the architecture 0 times, and the other thing 0 times.`;
+}
